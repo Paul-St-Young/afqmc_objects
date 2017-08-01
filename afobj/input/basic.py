9addc81d3f44130e84154fbcf42cd9cec5b25929
Paul Young <yyang173@illinois.edu>
2017-08-01 17:32:10 -0500

    format docstring

--- afobj/input/basic.py
+++ afobj/input/basic.py
@@ -3,3 +3,13 @@ class BasicInput(etree._ElementTree):
   def __init__(self):
     """ initialize BasicInput class """
     pass
+
+  def show(self,node):
+    """ print text representation of an xml node
+    Args:
+      node (lxml.etree.Element): the xml node to print
+    Returns:
+      None (None): effct print xml node
+    """
+    raw_text = etree.tostring(node,pretty_print=True)
+    print( raw_text.decode('ascii') )
