594f9a0e9e89e27ce86997165a42847a130f4b13
Paul Young <yyang173@illinois.edu>
2020-11-05 13:57:13 -0600

    x <-> et

--- afobj/basis/opt.py
+++ afobj/basis/opt.py
@@ -1,6 +1,11 @@
+import numpy as np
+
 def read_opt_log(flog):
   from qharv.reel import scalar_dat
-  df = scalar_dat.read(flog)
+  with open(flog, 'r') as f:
+    lines = f.readlines()
+  text = '\n'.join(lines[:-1])  # skip last line (cur. best)
+  df = scalar_dat.parse(text)
   xcols = get_xcols(df.columns)
   # drop finite difference runs (from basinhopping)
   nx = len(xcols)
@@ -28,3 +33,36 @@ def get_min(df):
   i = np.argmin(z)
   xi = df.iloc[i][xcols].values
   return z[i], xi
+
+def et_to_x(ab):
+  nab = len(ab)//2
+  assert len(ab) == 2*nab
+  xl = []
+  for i in range(nab):
+    a = ab[2*i]
+    b = ab[2*i+1]
+    nexpo = i+3
+    xl += [a*np.exp(-b*i) for i in range(nexpo)]
+  x = np.array(xl)
+  return x
+
+def x_to_et(x, ne, ml=10):
+  nx = len(x)
+  # find lmax
+  for lm in range(2, ml+1):
+    nx1 = (lm+4)*(lm-1)//2
+    if nx1 == nx//ne:
+      break
+  if lm >= ml:
+    msg = 'failed to find lmax'
+    raise RuntimeError(msg)
+  ab = []
+  for ie in range(ne):
+    for l in range(2, lm+1):
+      i0 = ie*nx1+(l-2)*l
+      x0 = x[i0]
+      x1 = x[i0+1]
+      a = x0
+      b = np.log(a/x1)
+      ab += [a, b]
+  return np.array(ab)
